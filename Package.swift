88aab289e22d8ddcce24f94324c3213457d4ade4
yutmr <yu.ampm@gmail.com>
2017-01-01 23:46:27 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  EntitlementBuilder
+//
+//  Created by Yu Tamura on 1/1/17.
+//  Copyright © 2017 Yu Tamura. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "EntitlementBuilder"
+)
---
+++ Sources/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  EntitlementBuilder
+//
+//  Created by Yu Tamura on 1/1/17.
+//  Copyright © 2017 Yu Tamura. All rights reserved.
+//
+
+print("Hello, world!")
